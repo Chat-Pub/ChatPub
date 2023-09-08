5153b0fb8f03aab2ac5a25ebfafbd65ec7e906a0
zino_macOS <jinho142857@gmail.com>
2023-09-08 12:42:59 +0900

    Minor changes.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ChatPub
+
+# To run codes
+```bash
+conda create -n scraper python=3.9
+conda activate scraper
+pip install -r requirements.txt
+python scraper.py
+```
---
+++ scraper.py
@@ -0,0 +1,14 @@
+"""
+The integrated process to crawl the entire data from 
+https://mediahub.seoul.go.kr/news/newsMain.do
+https://youth.seoul.go.kr/site/main/youth/politics/user/list
+and etc.
+
+The specific process will be determined later.
+"""
+
+
+import argparse
+import datetime
+
+parser = argparse.ArgumentParser()
