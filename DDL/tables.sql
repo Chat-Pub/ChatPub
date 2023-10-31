29683acfe305c6cc8623a3eedf9457a5ba55d0a0
zino_macOS <jinho142857@gmail.com>
2023-10-31 21:29:29 +0900

    Create create_DDL.ipynb

--- DDL/tables.sql
+++ DDL/tables.sql
@@ -1,9 +1,23 @@
 CREATE TABLE `YP_all_overview` (
-  `YP` varchar(15) NOT NULL COMMENT 'CRIS CT ID',
-  `title` varchar(255) DEFAULT NULL,
-  `R-number` varchar(30) DEFAULT NULL,
-  `url` varchar(255) DEFAULT NULL,
+  `YP` varchar(15) NOT NULL COMMENT 'YP ID',
+  `title` varchar(50) DEFAULT NULL,
+  `R-number` varchar(15) DEFAULT NULL,
+  `url` varchar(50) DEFAULT NULL,
   `create_date` datetime NOT NULL,
   `update_date` datetime NOT NULL,
   PRIMARY KEY (`YP`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `YP_summary` (
+  `YP` varchar(15) NOT NULL COMMENT 'YP ID',
+  `policy_area` varchar(15) DEFAULT NULL,
+  `support_content` varchar(255) DEFAULT NULL,
+  `operation_period` varchar(255) DEFAULT NULL,
+  `application_period` varchar(255) DEFAULT NULL,
+  `supprot_scale` varchar(100) DEFAULT NULL,
+  `remarks` varchar(255) DEFAULT NULL, 
+  `create_date` datetime NOT NULL,
+  `update_date` datetime NOT NULL,
+  PRIMARY KEY (`YP`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
