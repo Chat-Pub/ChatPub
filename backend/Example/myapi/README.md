f91a986c373bddb606de3df4f436236c87f9f0da
JUNChangMin <jsjs1209@skku.edu>
2023-11-04 21:06:26 +0900

    Feat:copy front

---
+++ backend/Example/myapi/README.md
@@ -0,0 +1,4 @@
+# 이 Repo는 Fastapi 공부를 위한 Repo 입니다.
+
+# 현재 진행상황
+https://wikidocs.net/176225
--- backend/Example/myapi/main.py
+++ backend/Example/myapi/main.py
@@ -5,16 +5,19 @@ from domain.question import question_router
 
 app = FastAPI()
 
+# 오류의 내용은 복잡하지만 간단히 말해 CORS 정책에 의해 요청이 거부되었다는 말이다.
+# 즉, 프론트엔드에서 FastAPI 백엔드 서버로 호출이 불가능한 상황이다.
+# 이 오류는 FastAPI에 CORS 예외 URL을 등록하여 해결할 수 있다.
 origins = [
-    "http://localhost:8000",
+    "http://127.0.0.1:5173",    # 또는 "http://localhost:5173"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = origins,
-    allow_credentials = True,
-    allow_methods = ["*"],
-    allow_headers = ["*"],
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(question_router.router)
