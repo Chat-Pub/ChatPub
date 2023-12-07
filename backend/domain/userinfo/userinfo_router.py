caa3a9011964ca6d62835d68ae2cb6853d73083f
ãJunChangM <jsjs1209@g.skku.edu>
2023-12-07 15:44:57 +0900

    Fix user info update

--- backend/domain/userinfo/userinfo_router.py
+++ backend/domain/userinfo/userinfo_router.py
@@ -34,19 +34,21 @@ def user_info_create(_user_info_create: userinfo_schema.UserInfoCreate,
     userinfo_crud.create_user_info(db=db,user_info_create=_user_info_create,
                                   user=current_user)
 
-@router.put("/update", status_code=status.HTTP_204_NO_CONTENT)
+@router.post("/update", status_code=status.HTTP_204_NO_CONTENT)
 def user_info_update(_user_info_update: userinfo_schema.UserInfoUpdate,
                     db: Session = Depends(get_db),
                     current_user: User = Depends(get_current_user)):
     db_user_info = userinfo_crud.get_user_info(db=db, user_id=current_user.id)
-    if not db_user_info:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"{current_user.id}에 대한 정보가 없습니다.")
+
     if current_user.id != db_user_info.user_id:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail="수정 권한이 없습니다.")
     
-    userinfo_crud.update_user_info(db=db, db_user_info=db_user_info, user_info_update=_user_info_update)
+    if not db_user_info:
+        userinfo_crud.create_user_info(db=db,user_info_create=_user_info_update,
+                                  user=current_user)
+    else:
+        userinfo_crud.update_user_info(db=db, db_user_info=db_user_info, user_info_update=_user_info_update)
 
 
 
