0bc6b94785d60e7c53d4b258a7638147b16392b0
kangsan <rkdtks419@g.skku.edu>
2023-12-07 17:49:46 +0900

    Front work

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 
-
+import govmark from './assets/gov.png';
+import chatmenu from './assets/chat.svg';
+import searchmenu from './assets/search.svg';
+import homemenu from './assets/home.svg';
+import user from './assets/user.svg';
 import mainchat from './assets/mainchat.png';
 
  import addBtn from './assets/add-30.png';
- import homeIcon from './assets/home.svg';
- import chatimg from './assets/chat.svg';
- import searchimg from './assets/search.svg';
  import sendBtn from './assets/send.svg';
  import userIcon from './assets/user.svg';
  import gptImgLogo from './assets/chatgptLogo.svg';
  import editIcon from './assets/edit.svg';
  import {useEffect, useRef,useState} from 'react';
+ import { Link } from 'react-router-dom';
 
  var selectedFolderId = 0;
 
@@ -234,8 +236,39 @@ function App() {
 
   return (
     <div className="App">
+      <div className="Sidebar" style={{width: 66, height: '100vh', paddingTop: 16, paddingBottom: 24, paddingLeft: 16, paddingRight: 16, left: 0, top: -1, position: 'absolute', background: 'white', boxShadow: '0px 0px 24px rgba(0, 0, 0, 0.08)', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', gap: 41, display: 'inline-flex'}}>
+        <div className="Govmark" style={{width: 69, height: 68, justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+          <img className="Govmark" style={{width: 50, height: 50}} src={govmark} alt ="Govmenu"/>
+        </div>
+        <div className="MainMenu" style={{width: 54, height: 254, flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', display: 'flex'}}>
+          <div style={{width: 54, height: 56.66, paddingTop: 4.76, paddingBottom: 4.74, paddingLeft: 4.50, paddingRight: 4.50, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+            <Link to = "/HomePage">
+              <img className="HomeMenu" style={{width: 40, height: 68 }} src={homemenu} alt ="Homemenu"/>
+            </Link>
+          </div>
+          <div style={{width: 54, height: 59.09, marginLeft:'11px', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+            <div style={{width: 54, height: 59.09, position: 'relative', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'flex'}}>
+                  <Link to = "/App">
+                    <img className="ChatMenu" style={{width: 40, height: 68}} src={chatmenu} alt ="ChatMenu"/>
+                  </Link>
+            </div>
+          </div>
+          <div style={{width: 54, height: 56.66, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+            <div style={{width: 54, height: 56.66, paddingLeft: 4.50, paddingRight: 4.50, paddingTop: 4.72, paddingBottom: 4.72, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+              <Link to = "/Search">
+                <img className="SearchMenu" style={{width: 40, height: 68}} src={searchmenu} alt ="SearchMenu"/>   
+              </Link>
+            </div>         
+          </div>
+        </div>
+          <div className ="userIcon" style={{ position:'absolute',bottom: 0, width: '100%', background: 'white', zIndex: 1000, display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '8px' }}>
+              <Link to ="/Login">
+                <img className="UserIcon" style={{width: 45, height: 68}} src={user} alt ="UserIcon"/>      
+              </Link>
+          </div>
+      </div>
       {/* side bar */}
-      <div className="sidebar">
+      <div className="Chat Menu" style={{margin:'0px 0px 0px 0px'}}>
         <div className = "upperSide">
           <div className='upperSideTop'><img src = {mainchat} alt ="Logo" className='logo'/><span className='brand'>Chat Pub</span></div>
           <div>
@@ -296,12 +329,7 @@ function App() {
           
           
        </div>
-        <div className = "lowerSide">
-          {/* menu - onClick send to each pages */}
-          <div className='listItems'><img src= {homeIcon} alt ="" className='listItemsImg'/>Home</div>
-          <div className='listItems'><img src= {chatimg} alt ="" className='listItemsImg'/>Chat</div>
-          <div className='listItems'><img src= {searchimg} alt ="" className='listItemsImg'/>Search</div>
-        </div>
+        <div className = "lowerSide"></div>
       </div>
 
       {/* main */}
